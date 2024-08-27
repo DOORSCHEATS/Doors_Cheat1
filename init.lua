edc8bc6536496b2ee8686734107ff96903a5153a
DOORSCHEATS <bcgperformance.contact@gmail.com>
2024-08-27 09:22:51 +0200

    create1

---
+++ init.lua
@@ -0,0 +1,36 @@
+local MHubUI = game.Players.LocalPlayer.PlayerGui:FindFirstChild("MHub")
+
+MHubUI = Instance.new("ScreenGui", game.Players.LocalPlayer.PlayerGui)
+
+MHubUI.IgnoreGuiInset = true
+MHubUI.Enabled = true
+
+local MainFrame = Instance.new("Frame", MHubUI)
+MainFrame.Name = "Main"
+
+MainFrame.Size = UDim2.fromScale(0.301, 0.741)
+MainFrame.Position = UDim2.fromScale(0.5, 0.5)
+MainFrame.AnchorPoint = Vector2.new(0.5, 0.5)
+
+MainFrame.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
+MainFrame.BorderColor3 = Color3.fromRGB(0, 85, 255)
+MainFrame.BorderSizePixel = 2
+MainFrame.Visible = true
+
+local TitleMain = Instance.new("TextLabel", MainFrame)
+
+TitleMain.Name = "Title"
+TitleMain.BackgroundTransparency = 1
+TitleMain.Size = UDim2.fromScale(0.97, 0.063)
+TitleMain.Position = UDim2.fromScale(0.015, 0)
+
+TitleMain.Text = string.format("MHub DOORS | Welcome : %s", game.Players.LocalPlayer.Name)
+
+game.UserInputService.InputBegan:Connect(function(Input)
+	if Input.KeyCode == Enum.KeyCode.Home then
+		if MHubUI then
+			MHubUI:Destroy()
+		end
+	end
+
+end)
